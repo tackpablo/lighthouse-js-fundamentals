c4258cc5f236de92e620c96b63f7ced3db159479
Pablo Tack <tackpablo@gmail.com>
2021-12-17 10:44:40 -0800

    for and while loop update

--- packing-list.js
+++ packing-list.js
@@ -8,8 +8,15 @@ const packingList = [
   "cups",
   "cooking utensils",
 ];
-
+//for loop
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
+}
+//while loop
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
 }
